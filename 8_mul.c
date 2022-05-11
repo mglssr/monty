891a4d708f33117bc7713869a8011470304352b7
MaRS-208 <manuela.riossosa@gmail.com>
2022-05-11 15:49:08 -0700

    added 8.mul

---
+++ 8_mul.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * f_add - adds the top two elements of the stack 
+ * @stack: stack
+ * @line_number: line of code
+ * Return: 0
+ */
+
+void *f_add(stack_t **stack, unsigned int line_number)
+{
+  stack_t bp, lp;
+  bp = *stack;
+  
+  if (bp)
+    lp = bp->next;
+  else
+  {
+    printf("L<%d>: %s\n", line_number, "can't add, stack too short");
+    exit(EXIT_FAILURE);
+  }
+  if (!lp)
+    printf("L<%d>: %s\n", line_number, "can't add, stack too short");
+    exit(EXIT_FAILURE);   
+  else
+    {
+      lp->n *= bp->n;
+      lp->prev = NULL;
+      free(bp);
+      *stack = lp;
+    }
+
+}
