f198ae26a094267b8344e2de351201d8fd4c63f9
mglssr <iglesiasmaia6@gmail.com>
2022-05-11 13:23:46 -0700

    added error exits for arguments number, file permissions, malloc

--- pruebas_de_mai/main.c
+++ pruebas_de_mai/main.c
@@ -1,15 +1,31 @@
 #include "monty.h"
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
 	const char *filename = argv[1];
 	size_t buffersize = 0;
-	char *buffer = NULL, *token;
-	char **opi = malloc(2 * sizeof(char *));
+	char *buffer = NULL, *token, **opi;
 	char *delim = " \t";
 	int i;
 	
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit (EXIT_FAILURE);
+	}
 	FILE* input_ = fopen(filename, "r");
+	
+	if (input_ == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+	opi = malloc(2 * sizeof(char *));
+	if(!opi)
+	{
+		fprintf(stderr, "Error: malloc failedi\n");
+		exit (EXIT_FAILURE);
+	}
 	while (getline(&buffer, &buffersize, input_) != -1)
 	{
 		token = strtok(buffer, "\n");
