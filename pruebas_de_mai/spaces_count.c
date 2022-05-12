4c49cfbb4aed47a236f0e6bbcd4fef455e98b240
mglssr <iglesiasmaia6@gmail.com>
2022-05-12 13:19:05 -0700

    added the match function and moved main function

--- pruebas_de_mai/spaces_count.c
+++ pruebas_de_mai/spaces_count.c
@@ -32,3 +32,38 @@ int spaces_count(char *buffer)
 	count += 2;
 	return (count);
 }
+
+/**
+* matching_func - function that selects the correct function to apply for the opcode
+* @opcode: argument that we are look for his function  
+* Return: a pointer to the function hat corresponds (or not) to the opcode
+*/
+
+void (matching_func(const char *opcode))(stack_t **stack, unsigned int line_number);
+{
+	int i = 0;
+
+	instructions_t selector[] = {
+		{'push', f_push},
+		{'pall', f_pall},
+		{'pint', f_pint},
+		{'pop', f_pop},
+		{'swap', f_swap},
+		{'add', f_add},
+		{'nop', f_nop},
+		{'sub', f_sub},
+		{'div', f_div},
+		{'mul', f_mul},
+		{'mod', f_mod},
+		{NULL, NULL}
+	};
+
+	while (selector[i].opcode != NULL)
+	{
+		if(selector[i].opcode == *opcode)
+			break;
+		i++;
+	}
+
+	return(selector[i].f);
+}
